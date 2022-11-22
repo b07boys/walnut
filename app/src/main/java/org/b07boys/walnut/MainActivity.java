54b5b39410b660e02a396c4feddf1eb819c9ce99
Nathanael Robbins <nejrobbins@gmail.com>
2022-11-22 16:40:25 -0500

    Login -> Homepage navigation flow

--- app/src/main/java/org/b07boys/walnut/MainActivity.java
+++ app/src/main/java/org/b07boys/walnut/MainActivity.java
@@ -1,8 +1,15 @@
 package org.b07boys.walnut;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
