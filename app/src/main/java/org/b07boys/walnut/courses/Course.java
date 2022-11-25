229b380bbd601f1e2f494792879f148a9ed38a5e
KingLouis-CLXXII <30333055+KingLouis-CLXXII@users.noreply.github.com>
2022-11-25 12:25:21 -0500

    actually fixed conflicts this time

--- app/src/main/java/org/b07boys/walnut/courses/Course.java
+++ app/src/main/java/org/b07boys/walnut/courses/Course.java
@@ -1,4 +1,4 @@
-package org.b07boys.walnut;
+package org.b07boys.walnut.courses;
 
 import androidx.annotation.NonNull;
 
--- app/src/main/java/org/b07boys/walnut/courses/GenerateTimeline.java
+++ app/src/main/java/org/b07boys/walnut/courses/GenerateTimeline.java
@@ -1,24 +1,31 @@
-package org.b07boys.walnut;
+package org.b07boys.walnut.courses;
 
 import java.util.ArrayList;
 
+
 public class GenerateTimeline {
 
+    /**
+     *
+     * @param timeline
+     * @param desiredCourses
+     * @param takenCourses
+     * @param maxCoursesPerSem
+     * @return
+     */
     public static ArrayList<ArrayList<ArrayList<Course>>> generate(
             ArrayList<ArrayList<Course>> timeline, ArrayList<Course> desiredCourses,
             ArrayList<Course> takenCourses, int maxCoursesPerSem){
 
-        ArrayList<ArrayList<ArrayList<Course>>> timelinePossibilities;
+        ArrayList<ArrayList<ArrayList<Course>>> timelinePossibilities = new ArrayList<>();
+        gen(timeline,desiredCourses,takenCourses,maxCoursesPerSem);
 
-
-
-        return null;
+        return timelinePossibilities;
     }
 
-    private static ArrayList<ArrayList<Course>> gen(
+    private static void gen(
             ArrayList<ArrayList<Course>> timeline, ArrayList<Course> desiredCourses,
             ArrayList<Course> takenCourses, int maxCoursesPerSem){
 
-        return null;
     }
 }
