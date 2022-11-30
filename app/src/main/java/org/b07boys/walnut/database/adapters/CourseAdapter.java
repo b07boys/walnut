a3ef9fa89fda8dbe0618759bd358085537c6d0d7
Lucas I <lucasi@Lucass-MacBook-Pro.local>
2022-11-29 22:39:31 -0500

    rebasing

--- app/src/main/java/org/b07boys/walnut/database/adapters/CourseAdapter.java
+++ app/src/main/java/org/b07boys/walnut/database/adapters/CourseAdapter.java
@@ -1,4 +1,13 @@
 package org.b07boys.walnut.database.adapters;
 
 public class CourseAdapter {
+
+    public CourseAdapter() {
+
+    }
+
+    public CourseAdapter(String code, String name, Session[] offeringSessions, ) {
+
+    }
+
 }
