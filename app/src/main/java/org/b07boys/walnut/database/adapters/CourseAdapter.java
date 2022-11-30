ac77f9244fc6c3c25dae5a939e321e7f6551d2c9
Lucas I <lucasi@Lucass-MacBook-Pro.local>
2022-11-29 22:48:40 -0500

    fixed enum declaration

--- app/src/main/java/org/b07boys/walnut/database/adapters/CourseAdapter.java
+++ app/src/main/java/org/b07boys/walnut/database/adapters/CourseAdapter.java
@@ -1,12 +1,14 @@
 package org.b07boys.walnut.database.adapters;
 
+import org.b07boys.walnut.courses.SessionType;
+
 public class CourseAdapter {
 
     public CourseAdapter() {
 
     }
 
-    public CourseAdapter(String code, String name, Session[] offeringSessions, ) {
+    public CourseAdapter(String code, String name, SessionType[] offeringSessions) {
 
     }
 
