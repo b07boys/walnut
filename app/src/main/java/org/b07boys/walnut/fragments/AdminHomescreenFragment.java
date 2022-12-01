ae9b283b25f5b3dab453e83377af65bc18649209
Lucas I <lucasi@Lucass-MacBook-Pro.local>
2022-11-30 22:35:45 -0500

    Add popup

--- app/src/main/java/org/b07boys/walnut/fragments/AdminHomescreenFragment.java
+++ app/src/main/java/org/b07boys/walnut/fragments/AdminHomescreenFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -61,8 +62,13 @@ public class AdminHomescreenFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        }
+        binding.floatingActionButton.setOnClickListener(viewButton -> {
+            DialogFragment addPopUp = new AddCoursePopUpFragment();
+            addPopUp.show(getActivity().getSupportFragmentManager(), "addPopUp");
+        });
 
     }
+
+}
 
 
